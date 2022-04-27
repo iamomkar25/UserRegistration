785fdb37e92058b3a40099a480def4b15b4dc363
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-28 00:24:18 +0530

    adding-firstName-validation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using user_registration;
+namespace user_registration
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
+        }
+    }
+}
