743067b97778f460b607d17679be0a2a660ebc37
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-28 00:39:09 +0530

    adding-email-validation

--- RegexSample.cs
+++ RegexSample.cs
@@ -18,6 +18,9 @@ namespace user_registration
 
             Console.WriteLine("Validating The Last Name");
             ValidatingLastName();
+
+            Console.WriteLine("Validating Email Address");
+            ValidatingEmailId();
         }
 
 
@@ -51,6 +54,24 @@ namespace user_registration
             else
             {
                 Console.WriteLine("Invalid");
+            }
+      
+       }
+
+        public void ValidatingEmailId()
+        {
+            string emailPattern = (@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+            Regex regex = new Regex(emailPattern);
+            Console.WriteLine("Enter email Id");
+            string emails = Console.ReadLine();
+            bool res = regex.IsMatch(emails);
+            if (res)
+            {
+                Console.WriteLine("valid email address");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a Valid Email!");
             }
         }
     }
