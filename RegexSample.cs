c52fe5e63c3f2dd7d820d5da66038dfd3e563997
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-04-28 00:42:30 +0530

    adding-phoneNumber-validation

--- RegexSample.cs
+++ RegexSample.cs
@@ -21,6 +21,9 @@ namespace user_registration
 
             Console.WriteLine("Validating Email Address");
             ValidatingEmailId();
+
+            Console.WriteLine("Validating Phone Number");
+            ValidatingPhoneNum();
         }
 
 
@@ -73,6 +76,25 @@ namespace user_registration
             {
                 Console.WriteLine("Please enter a Valid Email!");
             }
+        }
+
+        public void ValidatingPhoneNum()
+        {
+            string phoneNumPattern = @"^[0-9]+[\s]+[0-9]{10}$";
+            Regex regex = new Regex(phoneNumPattern);
+            Console.WriteLine("Enter valid Phone Number");
+            string phoneNumber = Console.ReadLine();
+            bool res = regex.IsMatch(phoneNumber);
+            if (res)
+            {
+                Console.WriteLine("valid email address");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a Valid Email!");
+            }
+
+
         }
     }
 }
